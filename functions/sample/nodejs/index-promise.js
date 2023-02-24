a51e089fa1911b0a33571225235e741a6af9dc1f
lsalazar <luis.salazar.zen@gmail.com>
2023-02-24 11:08:18 -0500

    Lab 4 Week 3

--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -6,6 +6,73 @@ const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
+    return new Promise(function (resolve, reject) {
+        const { CloudantV1 } = require('@ibm-cloud/cloudant');
+        const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+        const authenticator = new IamAuthenticator({ apikey: 'NY832ImwcVT3MtWN7Nf8Tddl4JHF32ZJrxb1oCigK5P7' })
+        const cloudant = CloudantV1.newInstance({authenticator: authenticator});
+        cloudant.setServiceUrl('https://47758dc9-211d-4a78-a038-617ad9e1338a-bluemix.cloudantnosqldb.appdomain.cloud');
+        if (params.st) {
+            cloudant.postFind({
+                db: 'dealerships',
+                selector: {
+                    st: params.st
+                }
+            })
+            .then((result)=>{
+                let code = 200;
+                if (result.result.docs.length == 0) {
+                    code = 404;
+                }
+                resolve({
+                    statusCode: code,
+                    headers: { 'Content-Type': 'application/json' },
+                    body: result.result.docs
+                });
+            }).catch((err)=>{
+                reject(err);
+            })
+        } else if (params.id) {
+            id = parseInt(params.dealerId)
+            cloudant.postFind({
+                db: 'dealerships',
+                selector: {
+                    id: parseInt(params.id)
+                }
+            })
+            .then((result)=>{
+                let code = 200;
+                if (result.result.docs.length == 0) {
+                    code = 404;
+                }
+                resolve({
+                    statusCode: code,
+                    headers: { 'Content-Type': 'application/json' },
+                    body: result.result.docs
+                });
+            }).catch((err)=>{
+                reject(err);
+            })
+        } else {
+            cloudant.postAllDocs({ db: 'dealerships', includeDocs: true, limit: 10 })            
+            .then((result)=>{
+                let code = 200;
+                if (result.result.rows.length == 0) {
+                    code = 404;
+                }
+                resolve({
+                    statusCode: code,
+                    headers: { 'Content-Type': 'application/json' },
+                    body: result.result.rows
+                });
+            }).catch((err)=>{
+                reject(err);
+            })
+        }
+    })
+}
+
+function main(params) {
 
     const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
     const cloudant = CloudantV1.newInstance({
--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -33,3 +33,56 @@ def main(param_dict):
         return {"error": err}
 
     return {"dbs": client.all_dbs()}
+
+# import sys
+# from ibmcloudant.cloudant_v1 import CloudantV1
+# from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict):
+    authenticator = IAMAuthenticator("NY832ImwcVT3MtWN7Nf8Tddl4JHF32ZJrxb1oCigK5P7")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://47758dc9-211d-4a78-a038-617ad9e1338a-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_find(
+        db='reviews',
+        selector={'dealership': {'$eq': int(dict["id"])}},
+        ).get_result()
+        
+    try:
+        result= {
+        'headers': {'Content-Type':'application/json'},
+        'body': {'data':response}
+        }
+        if(len(result['body']['data']['docs'])==0):
+            return {
+                'statusCode': 404,
+                'message': 'Dealer ID does not exist'
+            }
+        else:
+            return result
+    except:
+        return {
+        'statusCode': 500,
+        'message': 'Something went wrong'
+        }
+
+# import sys
+# from ibmcloudant.cloudant_v1 import CloudantV1
+# from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+def main(dict):
+    authenticator = IAMAuthenticator("NY832ImwcVT3MtWN7Nf8Tddl4JHF32ZJrxb1oCigK5P7")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://47758dc9-211d-4a78-a038-617ad9e1338a-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_document(db='reviews', document=dict["review"]).get_result()
+    
+    try:
+        result= {
+        'headers': {'Content-Type':'application/json'},
+        'body': {'data':response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 500,
+        'message': 'Something went wrong'
+        }
